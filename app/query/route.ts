6e2fde3d50a33f8fafb4d318d978695facfd3cf1
Pintu-blufig <51435256+pintukumar123-11@users.noreply.github.com>
2024-12-11 22:19:40 +0530

    Update route.ts

--- app/query/route.ts
+++ app/query/route.ts
@@ -18,9 +18,9 @@ export async function GET() {
     message:
       'Uncomment this file and remove this line. You can delete this file when you are finished.',
   });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
 }
