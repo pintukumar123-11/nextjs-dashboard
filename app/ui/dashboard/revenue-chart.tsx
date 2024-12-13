a2b3fa12cecc83a3f76ffd957362b7fa354a19d9
Pintu-blufig <51435256+pintukumar123-11@users.noreply.github.com>
2024-12-13 16:16:15 +0530

    Update revenue-chart.tsx

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -17,7 +17,6 @@ export default async function RevenueChart({
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
-  // const { yAxisLabels, topLabel } = generateYAxis(revenue);
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
   if (!revenue || revenue.length === 0) {
